58b3c72619841dfd92c2064147c0522cc4b656ac
Aram Menocci <aramsm3@gmail.com>
2019-05-19 14:56:27 -0300

    Created contacts controller

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,29 @@
+class ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all.order("created_at DESC")
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+
+    if @contact.save
+      redirect_to @contact
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:email, :name)
+  end
+end
