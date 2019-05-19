95c27cdad4b9dac0c5b90dc9e4ae00cdac53e2c5
Aram Menocci <aramsm3@gmail.com>
2019-05-19 14:53:37 -0300

    Made contacts email unique

--- db/migrate/20190519044800_create_contacts.rb
+++ db/migrate/20190519044800_create_contacts.rb
@@ -1,10 +1,12 @@
 class CreateContacts < ActiveRecord::Migration[5.1]
   def change
     create_table :contacts do |t|
-      t.string :email, :primary_key
+      t.string :email, null: false
       t.string :name, null: false
 
       t.timestamps
     end
+
+    add_index :contacts, :email, unique: true
   end
 end
