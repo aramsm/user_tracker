163d277ba8e196a0f38073ca7ae7e5e3cfd2fcbd
Aram S. Menocci <amenocci@aliancahospitalar.com>
2019-05-21 13:47:38 -0300

    Created simple tests

--- test/models/contact_test.rb
+++ test/models/contact_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class ContactTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create a contact" do
+    name = "test"
+    email = "test@test.com"
+    contact = Contact.new(name: name, email: email)
+
+    assert contact.valid?
+    assert contact.save
+  end
+
+  test "fails with invalid email format" do
+    name = "test"
+    email = "test@"
+    contact = Contact.new(name: name, email: email)
+
+    refute contact.valid?
+    refute contact.save
+  end
+
+  teste "fails to create a contact without params" do
+    contact = Contact.new()
+
+    refute contact.valid?
+    refute contact.save
+  end
 end
--- test/models/visit_test.rb
+++ test/models/visit_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class VisitTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create a visit" do
+    guid = "54820484"
+    url = "/"
+    visit = Visit.new(guid: guid, url: url)
+
+    assert visit.valid?
+    assert visit.save
+  end
+
+  teste "fails to create a visit with invalid params" do
+    visit = Visit.new()
+
+    refute visit.valid?
+    refute visit.save
+  end
 end
